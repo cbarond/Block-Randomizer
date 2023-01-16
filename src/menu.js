67e89a80c07d21b2e91ffc4fc50ffa1d3c3990b5
Caelan Barondess <cbarondess19@gmail.com>
2023-01-15 18:08:46 -0700

    Add instructions

--- src/menu.js
+++ src/menu.js
@@ -6,6 +6,16 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active')
 })
 
-// window.onload = function startup() {
-//     alert("This project is a work in progress and may have bugs.\n\nInstructions:");
-// }
+let instructions = `This project is a work in progress and may have bugs.\n
+Instructions:
+- Insert how many rows and columns you want
+- Select whether you want corners of the same color to touch
+- For each item you want, fill in a color, quantity, and name
+- Press the "Generate" button to create the grid\n
+Notes:
+- You don't need to fill in all the slots for items, only what you need.
+- It may not create a proper grid the first time. Check your quantities and try running it a few more times.`
+
+window.onload = function startup() {
+    alert(instructions);
+}
